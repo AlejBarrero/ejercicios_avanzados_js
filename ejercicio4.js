f5f9549c249f0f2e79ac4ee48b4888649d4c6563
Alejandro Barrero <krote95@gmail.com>
2025-02-02 15:53:00 +0100

    Ejercicios Bucles y Funciones JS

--- ejercicio4.js
+++ ejercicio4.js
@@ -2,8 +2,13 @@
 Haz varios ejemplos y compruébalos. */
 
 function findArrayIndex(array, text) {
-    //Tu codigo
+    for (let item of array) {
+            if (text === item) {
+                console.log("La posicion del elemento es: " + array.indexOf(item));
+        }
+    }
 }
+
 
 const mainCharacters = [
     "Luke",
@@ -15,4 +20,11 @@ const mainCharacters = [
     "Obi-Wan",
 ];
 
-findArrayIndex(mainCharacters, "");
+findArrayIndex(mainCharacters, "Anakin");
+
+/* ********************************************************** 
+Usando la función anterior benefíciate de poder conocer el **indice** del array para crear una función llamada **removeItem** que, pasándole un array y un texto como parámetros (los mismos parámetros que en el anterior ejercicio), llame a la función anteriormente creada findArrayIndex y obtén el indice para posteriormente usar la función de javascript .splice() para eliminar el elemento del array.
+Es decir, tienes que crear una función que elimine elementos del array y retorne el nuevo array sin el elemento, apoyándote en **findArrayIndex**.
+Finalmente retorna el array.
+De nuevo haz varios ejemplos para practicar y comprueba que funcionan correctamente. */
+

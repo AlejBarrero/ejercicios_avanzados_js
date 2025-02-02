691cbc8cf5ff85af687742caacf856984ea7cc77
Alejandro Barrero <krote95@gmail.com>
2025-02-02 15:05:32 +0100

    Ejercicios Bucles y Funciones JS

---
+++ ejercicio4.js
@@ -0,0 +1,16 @@
+/* 1. Crea una función llamada findArrayIndex que reciba como parametros un array de textos y un texto y devuelve la posición del array cuando el valor del array sea igual al valor del texto que enviaste como parámetro.
+Haz varios ejemplos y compruébalos. */
+
+function findArrayIndex(array, text) {
+    //Tu codigo
+}
+
+const mainCharacters = [
+    "Luke",
+    "Leia",
+    "Han Solo",
+    "Chewbacca",
+    "Rey",
+    "Anakin",
+    "Obi-Wan",
+];
